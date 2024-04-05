661cb2e08d6a868a992cbf61bcf59682081150af
polawich <toniceee45@gmail.com>
2024-04-05 11:40:07 +0700

    add file

--- src/ratings/databases/script.sh
+++ src/ratings/databases/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -e
-mongoimport --host localhost --db ratings \
-  --collection ratings --drop --file /docker-entrypoint-initdb.d/ratings_data.json
+mongoimport --host localhost --username $MONGODB_EXTRA_USERNAMES --password $MONGODB_EXTRA_PASSWORDS \
+  --db $MONGODB_EXTRA_DATABASES --collection ratings --drop --file /docker-entrypoint-initdb.d/ratings_data.json
